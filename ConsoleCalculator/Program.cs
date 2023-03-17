4474a8c9f7fb9c6dc13b210b85e15ceaae314dd9
Victorien Sukarieh <vsukarieh@gmail.com>
2023-03-17 18:42:35 +0200

    test commit

--- ConsoleCalculator/Program.cs
+++ ConsoleCalculator/Program.cs
@@ -3,6 +3,7 @@
     static void Main(string[] args)
     {
         bool endApp = false;
+
         // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
